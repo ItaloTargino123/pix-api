eb076949c69e0ced30293563718058921c90a90d
apoooks <italo.dasilva@rolecatcher.com>
2026-01-16 10:33:50 -0300

    pix views, retornar limite de mensagens baseadi no header

--- src/pix/views.py
+++ src/pix/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.conf import settings
+from faker import Faker
 
-# Create your views here.
+fake = Faker('pt_BR')
+
+
+def get_message_limit(request) -> int:
+    """Retorna limite de mensagens baseado no header Accept ou query param."""
+    accept = request.headers.get('Accept', '')
+    format_param = request.query_params.get('format', '')
+    
+    if 'multipart/json' in accept or format_param == 'multipart':
+        return settings.PIX_MAX_MESSAGES_PER_REQUEST
+    return 1
