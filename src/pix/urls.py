bfae208e7e67b32f2f9a1ffabf0c254783f49a74
apoooks <italo.dasilva@rolecatcher.com>
2026-01-16 14:39:46 -0300

    urls para pix > views

--- src/pix/urls.py
+++ src/pix/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
+from .views import generate_messages, stream_start, stream_continue
 
-urlpatterns = []
+urlpatterns = [
+    # Stream endpoints
+    path('<str:ispb>/stream/start', stream_start, name='stream-start'),
+    path('<str:ispb>/stream/<str:interation_id>', stream_continue, name='stream-continue'),
+    # Utilitários
+    path('util/msgs/<str:ispb>/<int:quantity>/', generate_messages, name='generate-messages'),
+]
